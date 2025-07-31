30829020e5e2f009661823317783af50edd3b55e
Charles Faso <fasocharlie@gmail.com>
2025-07-31 19:42:53 -0400

    Add files via upload

---
+++ PasswordManager/menu.py
@@ -0,0 +1,235 @@
+import tkinter as tk # GUI
+import pyperclip # To copy to clipboard
+import password_checker # Password strength
+import random, string # Random password
+
+# Swap frams
+def show_frame(frame):
+    frame.tkraise()
+
+# Color password field based on password strength
+def color_password(entry, *args):
+    pwd = entry.get()
+    strength = password_checker.pwd_strength(pwd)
+    if len(pwd)==0:
+        entry.config(bg="white")
+    elif strength == "Strong":
+        entry.config(bg="lightgreen")
+    elif strength == "Moderate":
+        entry.config(bg="yellow")
+    else:
+        entry.config(bg="red")
+
+# Create main app window
+root = tk.Tk()
+root.title("Password Manager")
+root.geometry("300x200")
+root.rowconfigure(0, weight=1)
+root.columnconfigure(0, weight=1)
+root.resizable(False, False)
+
+
+# Frame 1: Login Screen 
+class LoginScreen(tk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.configure(bg="lightblue")
+
+        # Master label and entry
+        tk.Label(self, text="Enter Master Password:", font=("Arial", 14), bg="lightblue").pack(pady=10)
+        self.master_pwd = tk.Entry(self, show='*')
+        self.master_pwd.pack(pady=5)
+        self.login_btn = tk.Button(self, text="Login", command=self.login)
+        self.login_btn.pack(pady=5)
+
+    # Check password and go to home screen if correct
+    def login(self):
+        if self.master_pwd.get() == "test":
+            show_frame(home)
+        else:
+            self.login_btn.config(text="Try Again")
+
+
+# Frame 2: Home 
+class HomeScreen(tk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.configure(bg="lightblue")
+
+        # Welcome title
+        self.title = tk.Label(self, text="Welcome!", font=("Arial", 14), bg="lightblue")
+        self.title.pack(pady=10)
+
+        tk.Button(self, text="New Password", command=lambda: show_frame(insertion)).pack(pady=10)
+        tk.Button(self, text="Find Password", command=lambda: show_frame(search)).pack(pady=10)
+
+
+# Frame 3: Insertion
+class InsertionScreen(tk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.configure(bg="lightblue")
+        self.grid()
+
+        # Title
+        self.title = tk.Label(self, text="Enter Details", font=("Arial", 14), bg="lightblue").grid(row=0, column=0)
+
+        # Website label and entry
+        tk.Label(self, text = "Website: ", bg="lightblue", font=("Arial", 12), pady=10, padx=5).grid(row=1, column=0)
+        self.website = tk.Entry(self)
+        self.website.grid(row=1, column=1)
+
+        # Username label and entry
+        tk.Label(self, text="Username: ", bg="lightblue", font=("Arial", 12), pady=10, padx=5).grid(row=2, column=0)
+        self.username = tk.Entry(self)
+        self.username.grid(row=2, column=1)
+
+        # Password label and entry with auto strength check
+        tk.Label(self, text="Password: ", bg="lightblue", font=("Arial", 12), pady=10, padx=5).grid(row=3, column=0)
+        self.password = tk.Entry(self)
+        self.password.grid(row=3, column=1)
+        self.password.bind("<KeyRelease>", lambda event: color_password(self.password))
+
+        # Feedback Button
+        self.feedback_btn = tk.Button(self, text="Feedback", command=self.feedback_func, pady=5, width=10)
+        self.feedback_btn.grid(row=4, column=1)
+
+        # Add password button
+        self.insert_btn = tk.Button(self, text="Add Password", command=self.insert, pady=5, width=10)
+        self.insert_btn.grid(row=0, column=1)
+
+        # Random password button
+        self.random_btn = tk.Button(self, text="Generate", command=self.generate_password, pady=5, width=5)
+        self.random_btn.grid(row=4, column=0)
+    
+    # Feedback button functions
+    def feedback_func(self):
+        feedback.set_password()
+        feedback.set_feedback()
+        show_frame(feedback)
+
+    # Generate password and show in password field
+    def generate_password(self):
+        LENGTH = 15
+        characters = string.ascii_letters + string.digits + string.punctuation
+        random_password = ''.join(random.choice(characters) for _ in range(LENGTH))
+
+        self.password.delete(0, tk.END)
+        self.password.insert(0, random_password)
+        color_password(self.password)
+
+        pyperclip.copy(random_password)
+        self.random_btn.config(text="Copied")
+
+    # Data base insertion if fields filled in
+    def insert(self):
+        # Reset field color
+        self.website.config(bg="white")
+        self.username.config(bg="white")
+        color_password(self.password)
+
+        if self.website.get() and self.username.get() and self.password.get():
+            # TODO make real database
+            data = {self.website.get(): {"username": self.username.get(), "password": self.password.get()}}
+            database.update(data)
+            
+            home.title.config(text="Password Added!")
+            self.insert_btn.config(text="Add Password")
+            show_frame(home)
+
+            # Reset field text
+            self.website.delete(0, tk.END)
+            self.username.delete(0, tk.END)
+            self.password.delete(0, tk.END)
+            self.password.config(bg="white")
+            self.random_btn.config(text="Generate")
+            self.feedback_btn.config(text="Feedback")
+        else:
+            # Change colors of fileds not filled in
+            if not self.website.get():
+                self.website.config(bg="lightcoral")
+            if not self.username.get():
+                self.username.config(bg="lightcoral")
+            if not self.password.get():
+                self.password.config(bg="lightcoral")
+            self.insert_btn.config(text="Try Again")
+
+
+# Frame 4: Feedback
+class FeedbackScreen(tk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.configure(bg="lightblue")
+        self.grid()
+
+        # Title
+        tk.Label(self, text="Password Feedback", font=("Arial", 14), bg="lightblue", width=30).grid(row=0, column=0, columnspan=2)
+
+        # Password Entry
+        self.password = tk.Entry(self, width=20)
+        self.password.grid(row=1, column=0)
+        self.password.bind("<KeyRelease>", lambda event: (color_password(self.password), self.set_feedback()))
+
+        # Feedback
+        self.feedback = tk.Label(self, font=("Arial", 11), bg="lightblue", wraplength=300, justify="left")
+        self.feedback.grid(row=2, column=0, columnspan=2)
+
+        # Return Button
+        tk.Button(self, text="Use", command=lambda: self.exit_feedback(), padx=10).grid(row=1, column=1)
+
+    # Initial password text and color
+    def set_password(self):
+        self.password.insert(0, insertion.password.get())
+        color_password(self.password)
+
+    # Set feedback
+    def set_feedback(self):
+        pwd = self.password.get()
+        if pwd:
+            feedback = password_checker.pwd_feedback(pwd)
+            strength = password_checker.pwd_strength(pwd)
+            score = password_checker.pwd_score(pwd)
+            if score >= 100:
+                score = "100+"
+            output = f"Strength: {strength}    Score: {score}\n{feedback}"
+            self.feedback.config(text=output)
+        else:
+            self.feedback.config(text="")
+
+    # Exit feedback and update fields
+    def exit_feedback(self):
+        pyperclip.copy(self.password.get())
+        insertion.feedback_btn.config(text="Copied")
+        insertion.password.delete(0, tk.END)
+        insertion.password.insert(0, self.password.get())
+        color_password(insertion.password)
+        self.password.delete(0, tk.END)
+        show_frame(insertion)
+        
+# Frame 5: Search
+class SearchScreen(tk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.configure(bg="lightblue")
+        self.grid()
+
+        # Title
+        self.title = tk.Label(self, text="Search", font=("Arial", 14), bg="lightblue")
+        self.title.grid(row=0, column=0)
+
+
+# Initialize
+login = LoginScreen(root)
+home = HomeScreen(root)
+insertion = InsertionScreen(root)
+feedback = FeedbackScreen(root)
+search = SearchScreen(root)
+database = {}
+
+# Stack frames
+for frame in (login, home, insertion, feedback, search):
+    frame.grid(row=0, column=0, sticky="nsew")
+
+# Begin
+show_frame(login)
+root.mainloop()
